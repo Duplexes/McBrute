31ba8c341035e31bf05814c6011c5376faf76c7f
Duplexes <63378937+Duplexes@users.noreply.github.com>
2022-11-08 16:25:44 -0800

    Firrst commit

--- mcscanner.py
+++ mcscanner.py
@@ -2,18 +2,25 @@ from mcstatus import JavaServer
 import os
 import math
 import threading
+from deta import Deta
 import time
 import argparse
 
 parser = argparse.ArgumentParser(description='get files for procesing')
 parser.add_argument("-i", "--inputfile", type=str, help="put in the file with all the server IP's")
-parser.add_argument("-o","--outputfile", type=str, help="the name of the file to put in the results")
-parser.add_argument("-p","--publicserverlist", type=str, help="put in the file with the public server list (public.txt)")
-parser.add_argument("-v","--version", type=str, default="", required=False, help="you can specify the minecarft server you wanna find")
+parser.add_argument("-o", "--outputfile", type=str, help="the name of the file to put in the results")
+parser.add_argument("-p", "--publicserverlist", type=str,
+                    help="put in the file with the public server list (public.txt)")
+parser.add_argument("-v", "--version", type=str, default="", required=False,
+                    help="you can specify the minecarft server you wanna find")
 args = parser.parse_args()
-
+print('Multithreaded mass minecraft server status checker by Footsiefat/Deathmonger, edited by @Duplexes#5466')
 masscan = []
-print('Multithreaded mass minecraft server status checker by Footsiefat/Deathmonger')
+print("Loading servers...")
+deta = Deta(input("Deta key: "))
+print("Starting threads...")
+print("Started! Keep this window open!")
+db = deta.Base("Mcscanner")
 
 time.sleep(1)
 
@@ -25,41 +32,42 @@ searchterm = args.version
 outfile = open(outputfile, 'a+')
 outfile.close
 
-fileHandler = open (inputfile, "r")
+fileHandler = open(inputfile, "r")
 listOfLines = fileHandler.readlines()
 fileHandler.close()
 
 for line in listOfLines:
     if line.strip()[0] != "#":
-        masscan.append(line.strip().split(' ',4)[3])
+        masscan.append(line.strip().split(' ', 4)[3])
 
 
-
-def split_array(L,n):
+def split_array(L, n):
     return [L[i::n] for i in range(n)]
 
 
-threads = int(input('How many threads do you want to use? (Recommended 20): '))
+threads = 400
 
 time.sleep(2)
 
 if len(masscan) < int(threads):
     threads = len(masscan)
 
-
 split = list(split_array(masscan, threads))
 
 exitFlag = 0
 
-class myThread (threading.Thread):
+
+class myThread(threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+
     def run(self):
-        print ("Starting Thread " + self.name)
+        print("Starting Thread " + self.name)
         print_time(self.name)
-        print ("Exiting Thread " + self.name)
+        print("Exiting Thread " + self.name)
+
 
 def print_time(threadName):
     for z in split[int(threadName)]:
@@ -67,21 +75,26 @@ def print_time(threadName):
             threadName.exit()
         try:
             ip = z
-            server = JavaServer(ip,25565)
+            server = JavaServer(ip, 25565)
             status = server.status()
+            query = server.query()
         except:
-            print("Failed to get status of: " + ip)
+            print("")
         else:
-            print("Found server: " + ip + " " + status.version.name + " " + str(status.players.online))
+            print("Found server: " + ip + " Version: " + status.version.name + " Online: " + str(status.players.online))
+            db.put({"ip": ip, "version": status.version.name, "online": status.players.online,
+                    "players": query.players.names, "motd": query.motd})
             if searchterm in status.version.name:
                 with open(outputfile) as f:
                     if ip not in f.read():
                         with open(publicserverlist) as g:
                             if ip not in g.read():
                                 text_file = open(outputfile, "a")
-                                text_file.write(ip + " " + status.version.name.replace(" ", "_") + " " + str(status.players.online))
+                                text_file.write(
+                                    ip + " " + status.version.name.replace(" ", "_") + " " + str(status.players.online))
                                 text_file.write(os.linesep)
                                 text_file.close()
+
 
 for x in range(threads):
     thread = myThread(x, str(x)).start()
